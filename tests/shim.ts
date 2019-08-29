9e7a0d117705f0639617b937748d6dcc47e777ec
李光耀 <ligyj@chanjet.com>
2019-08-29 21:16:24 +0800

    [chore] add global polyfill to jest

--- tests/shim.ts
+++ tests/shim.ts
@@ -3,5 +3,33 @@
  * 2017/11/22
  * lgy87@foxmail.com
  */
+import * as r from "ramda"
 import { JSDOM } from "jsdom"
-;(global as any).window = new JSDOM("", { url: "http://localhost" }).window
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      window: Window
+      document: Document
+      navigator: Navigator
+      requestAnimationFrame: (callback: any) => Timeout
+      cancelAnimationFrame: (id: Timeout) => void
+      Image: HTMLImageElement
+    }
+  }
+}
+
+const jsdom = new JSDOM("<!doctype html><html><body></body></html>")
+const { window } = jsdom
+function copyProps(src: any, target: any) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  })
+}
+
+global.window = window
+global.document = window.document
+global.requestAnimationFrame = r.partialRight(setTimeout, [0])
+global.cancelAnimationFrame = clearTimeout
+copyProps(window, global)
