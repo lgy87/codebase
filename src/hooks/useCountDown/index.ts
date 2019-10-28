34731cb7af94c084aed6680dd9228f8b5ecf916f
李光耀 <ligyj@chanjet.com>
2019-10-29 02:04:00 +0800

    [feat] add useCountDown, useImageSize, useInterval, usePrevious hooks

---
+++ src/hooks/useCountDown/index.ts
@@ -0,0 +1,39 @@
+import { useState, useCallback } from "react"
+import * as r from "ramda"
+
+import useInterval from "~/hooks/useInterval"
+
+const INTERVAL = 1000
+
+type SetFn = () => void
+type Result = {
+  current: number
+  reset: SetFn
+  resume: SetFn
+  pause: SetFn
+}
+
+export default function(from: number, to: number): Result {
+  const [current, setCurrent] = useState(from)
+  const [delay, setDelay] = useState<number | null>(INTERVAL)
+
+  const reset = useCallback(() => {
+    setCurrent(from)
+    setDelay(INTERVAL)
+  }, [from])
+
+  const pause = useCallback(() => setDelay(null), [])
+
+  const resume = useCallback(() => {
+    setDelay(INTERVAL)
+  }, [])
+
+  const countDown = useCallback(() => current > to && setCurrent(r.dec), [
+    current,
+    to,
+  ])
+
+  useInterval(countDown, delay)
+
+  return { current, reset, resume, pause }
+}
